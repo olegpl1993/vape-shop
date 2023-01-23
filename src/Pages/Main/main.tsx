91698dcbadfbb7063d8b729b509bfee85ee47ea2
olegpl1993 <olegpl1993@gmail.com>
2023-01-24 00:04:48 +0200

    feat: main page images

--- src/Pages/Main/main.tsx
+++ src/Pages/Main/main.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import './main.scss';
+import { NavLink } from 'react-router-dom';
 
 function Main() {
   return (
-    <div>Hello Main Page</div>
+    <div className="main">
+      <div className="imgContainer">
+        <NavLink to="/disposable">
+          <div className="disposableImgBox imgBox">Одноразки</div>
+        </NavLink>
+        <NavLink to="/vapes">
+          <div className="vapesImgBox imgBox">Вейпы</div>
+        </NavLink>
+        <NavLink to="/juice">
+          <div className="juiceImgBox imgBox">Жидкости</div>
+        </NavLink>
+        <NavLink to="/equipment">
+          <div className="equipmentImgBox imgBox">Комплектующие</div>
+        </NavLink>
+      </div>
+    </div>
   );
 }
 
