635a8bce4e5a401cd1a5fbc38ff0d21adb9d9e8e
olegpl1993 <olegpl1993@gmail.com>
2023-01-23 21:09:03 +0200

    feat: add vape and cart images

--- src/Header/header.tsx
+++ src/Header/header.tsx
@@ -1,29 +1,38 @@
 import React, { useMemo } from 'react';
 import { NavLink } from 'react-router-dom';
-import styles from './styles.module.scss';
+import './header.scss';
+import logoImg from '../img/logoImg.png';
 
 export default function Header() {
   const activeStyles = useMemo(
     () => (bool: boolean) => (bool
-      ? styles.header__navigation__link__active
-      : styles.header__navigation__link),
+      ? 'link__active'
+      : 'link'),
+    [],
+  );
+
+  const activeLogo = useMemo(
+    () => (bool: boolean) => (bool
+      ? 'logo__active'
+      : 'logo'),
     [],
   );
 
   return (
-    <div className={styles.header}>
-      <nav className={styles.header__navigation}>
-
-        <NavLink
-          className={({ isActive }: { isActive: boolean }) => activeStyles(isActive)}
-          to="/"
-        >
-          Main
-        </NavLink>
-        <NavLink
-          className={({ isActive }: { isActive: boolean }) => activeStyles(isActive)}
-          to="/other"
-        >
+    <div className="header">
+      <div className="logoRow">
+        <div className="logoBox">
+          <img src={logoImg} alt="logoVape" height={70} />
+          <NavLink className={({ isActive }: { isActive: boolean }) => activeLogo(isActive)} to="/">
+            Vape Family
+          </NavLink>
+        </div>
+        <div className="cart">
+          <div className="productInCart">0</div>
+        </div>
+      </div>
+      <nav className="navigation">
+        <NavLink className={({ isActive }: { isActive: boolean }) => activeStyles(isActive)} to="/other">
           Other page
         </NavLink>
       </nav>
