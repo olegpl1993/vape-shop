b7d80ac428e84abe2bceed0d7517884772175e61
olegpl1993 <olegpl1993@gmail.com>
2023-01-25 19:02:06 +0200

    feat: add data img

--- src/Pages/Disposable/disposable.tsx
+++ src/Pages/Disposable/disposable.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import './disposable.scss';
+import data from '../../data/database.json';
 
 function Disposable() {
+  console.log(data);
+  const { disposable } = data;
+  console.log(disposable);
   return (
-    <div>Одноразовые</div>
+    <div className="disposable">
+      <div className="title">Одноразовые</div>
+      <img src={disposable[0].images[0]} alt={disposable[0].title} />
+    </div>
   );
 }
 
