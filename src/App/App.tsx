c2f455075e9b5510cf22edddaadd09f822285890
olegpl1993 <olegpl1993@gmail.com>
2023-03-08 23:18:48 +0200

    refactor: improve App component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
-import { HashRouter } from 'react-router-dom';
-import RootRouter from './RootRouter/rootRouter';
+import React, { useState } from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Footer from '../Footer/footer';
+import Header from '../Header/header';
+import Cart from '../Pages/Cart/cart';
+import Disposable from '../Pages/Disposable/disposable';
+import Equipment from '../Pages/Equipment/equipment';
+import Juice from '../Pages/Juice/juice';
+import Main from '../Pages/Main/main';
+import PageNotFound from '../Pages/PageNotFound/pageNotFound';
+import Vapes from '../Pages/Vapes/vapes';
+import './App.scss';
 
 function App() {
+  const [cartState, setPosts] = useState([
+    { id: 2, number: 1 },
+    { id: 3, number: 2 },
+    { id: 7, number: 1 },
+  ]);
+  // калбек добавляет Product в cartState
+  const addProductToCart = (idProd: number) => {
+    setPosts([...cartState, { id: idProd, number: 1 }]);
+  };
+  console.log(cartState);
   return (
     <HashRouter>
-      <RootRouter />
+      <div className="wrapper">
+        <div className="headerBox"><Header cartState={cartState} /></div>
+        <div className="contentBox">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/disposable" element={<Disposable addProductToCart={addProductToCart} />} />
+            <Route path="/vapes" element={<Vapes />} />
+            <Route path="/juice" element={<Juice />} />
+            <Route path="/equipment" element={<Equipment />} />
+            <Route path="/cart" element={<Cart cartState={cartState} />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </div>
+        <div className="footerBox"><Footer /></div>
+      </div>
     </HashRouter>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import App from './App/App';
+import './nullstyle.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
