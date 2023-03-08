33a4c725bcddeaf6bc9ec59485daa07aedbfe3d7
olegpl1993 <olegpl1993@gmail.com>
2023-03-09 01:39:50 +0200

    fix: func addProductToCart

--- src/components/productCard/productCard.tsx
+++ src/components/productCard/productCard.tsx
@@ -10,14 +10,23 @@ interface Props {
 
 function ProductCard(props: Props) {
   const { id } = props;
-
   const cart = useAppSelector((state) => state.cart.cart);
   const dispatch = useAppDispatch();
+
   const addProductToCart = (idProd: number) => {
-    dispatch(changeCart([...cart, { id: idProd, number: 1 }]));
+    const prodIndex = cart.findIndex((el) => el.id === idProd);
+    if (prodIndex >= 0) {
+      const tempCart = [...cart];
+      tempCart[prodIndex] = { id: idProd, number: tempCart[prodIndex].number + 1 };
+      dispatch(changeCart(tempCart));
+    } else {
+      dispatch(changeCart([...cart, { id: idProd, number: 1 }]));
+    }
   };
+
   const { products } = data;
   const product = products[id];
+
   return (
     <div className="productCard">
       <div className="imgBox">
