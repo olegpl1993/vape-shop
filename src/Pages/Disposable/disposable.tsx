ec8f69fd475c60ecdac9094ecf4fe0494c8a19e0
olegpl1993 <olegpl1993@gmail.com>
2023-03-13 00:33:19 +0200

    feat: fetch to server

--- src/Pages/Disposable/disposable.tsx
+++ src/Pages/Disposable/disposable.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './disposable.scss';
 import data from '../../data/database';
 import ProductCard from '../../components/productCard/productCard';
+import { Product } from '../../types';
 
 function Disposable() {
+  const [result, setResult] = useState<Product[]>([]);
+  console.log('products = ', result);
+
+  useEffect(() => {
+    const api = async () => {
+      const url = 'https://vape-shop-api.glitch.me/api/products';
+      const res = await fetch(url);
+      const datas: Product[] = await res.json();
+      setResult(datas);
+    };
+    api();
+  }, []);
+
   const { products } = data;
   return (
     <div className="disposable">
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,16 @@ export interface CartElem {
   id: number;
   number: number;
 }
+
+export interface Product {
+  _id: string,
+  category: string,
+  title: string,
+  brand: string,
+  flavor: string,
+  price: number,
+  stock: number,
+  description: string,
+  images: string,
+  __v: number
+}
