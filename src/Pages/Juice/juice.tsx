93e067266732180177818d5f46d9dd961732bbe0
olegpl1993 <olegpl1993@gmail.com>
2023-03-19 21:05:13 +0200

    feat: vapes euip juice pages

--- src/Pages/Juice/juice.tsx
+++ src/Pages/Juice/juice.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ProductsBox from '../../components/productBox/productsBox';
+import { Product } from '../../types';
 
 function Juice() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [renderProducts, setRenderProducts] = useState<Product[]>(products);
+
+  const filterProducts = (searchText: string) => {
+    const searchName = products.filter(
+      (el) => el.category?.toLowerCase().includes(searchText.toLowerCase()),
+    );
+    const tempState = Array.from(new Set([...searchName]));
+    setRenderProducts(tempState);
+  };
+
+  useEffect(() => {
+    const api = async () => {
+      const url = 'https://vape-shop-api.glitch.me/api/products';
+      const res = await fetch(url);
+      const datas: Product[] = await res.json();
+      setProducts(datas);
+    };
+    api();
+  }, []);
+
+  useEffect(() => {
+    if (products.length > 0) filterProducts('juice');
+  }, [products]);
+
   return (
-    <div>Жидкости</div>
+    <div className="disposable">
+      <ProductsBox products={renderProducts} />
+    </div>
   );
 }
 
