78f19b8f758800298bb2af0b29cf8b1e5498d3e7
olegpl1993 <olegpl1993@gmail.com>
2023-03-19 18:34:31 +0200

    feat: add load spinner

--- src/Pages/Disposable/disposable.tsx
+++ src/Pages/Disposable/disposable.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import './disposable.scss';
 import ProductCard from '../../components/productCard/productCard';
 import { Product } from '../../types';
+import LoadingSpinner from '../../components/Spinner/LoadingSpinner';
 
 function Disposable() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -17,14 +18,18 @@ function Disposable() {
     api();
   }, []);
 
+  if (products.length === 0) {
+    return (
+      <div className="disposable">
+        <LoadingSpinner />
+      </div>
+    );
+  }
   return (
     <div className="disposable">
       <div className="productsBox">
         {products.map((product) => (
-          <ProductCard
-            key={product._id}
-            product={product}
-          />
+          <ProductCard key={product._id} product={product} />
         ))}
       </div>
     </div>
